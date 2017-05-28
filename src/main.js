f21d7fb2fda429b461c5918bb25e265bc18479d3
Alexey V Paramonov <me@paramonov.ru>
2017-05-28 18:38:50 +0300

    Current track image & title

---
+++ src/main.js
@@ -0,0 +1,45 @@
+import Vue from 'vue';
+import axios from 'axios';
+
+Vue.config.productionTip = false;
+
+Vue.component('radio-status-widget', {
+    props: [
+        'apiBase'
+    ],
+    data: () => {
+        return {
+            loaded: false,
+            trackImage: '',
+            trackMetadata: ''
+        };
+    },
+    methods: {
+        refreshTrackdata() {
+            axios.get(`${this.apiBase}/history?limit=1&offset=0&server=1`)
+                .then(response => {
+                    console.log("Response: ", response);
+                    if(response.data && response.data.results && response.data.results.length){
+                        const lastTrack = response.data.results[0];
+                        this.loaded = true;
+                        this.trackImage = lastTrack.img_url;
+                        this.trackMetadata = lastTrack.metadata;
+                    }
+                })
+                .catch(e => {
+                    console.log("Error:", e);
+                });
+        }
+    },
+    created() {
+        this.refreshTrackdata();
+        setInterval(()=>{
+            this.refreshTrackdata();
+        }, 5*1000);
+    },
+    template: '<div><div v-if="loaded"><img v-bind:src="trackImage" /><div>{{trackMetadata}}</div></div></div>'
+});
+
+new Vue({
+    el: '#app'
+});
